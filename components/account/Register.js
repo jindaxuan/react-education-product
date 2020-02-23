ff6d75e0d840e60cb1b436df1f970ce5cb91e3e5
Daxuan <jindaxuan@foxmail.com>
2020-02-23 10:40:21 +0800

    完成注册页面

--- components/account/Register.js
+++ components/account/Register.js
@@ -1,28 +1,118 @@
-
 // 导入antd中所有页面需要的组件
 import fetch from '../../kits/fetch'
 import css from '../../pages/account/login.less'
-import { Icon, Row, Col, Form, Input, Button,message } from 'antd'
+import { Icon, Row, Col, Form, Input, Button, message } from 'antd'
 
-const FormItem = Form.Item;
+const FormItem = Form.Item
 
+let intervalHander = null
+let timeout = 60
 
 class Register extends React.Component {
+  state = {
+    bttxt: '获取验证码', //  虎丘雁阵听歌吗按钮的文字
+    isdisabled: false //  控制获取验证码按钮是否可用
+  }
 
   // 定义一个register方法来注册用户
-  register= (e) =>{
+  register = e => {
     // 为了能够正常验证表单，需要两件事
     // 1、利用getFielDecrator进行表单合法性的检查
     // 2、在onSubmit指定的方法中利用this.props.form.validateField()来进行表单合法性检查的触发工作
-    e.preventDefault()   // 阻止了默认事件触发
-    this.props.form.validateFields((err,values)=>{
+    e.preventDefault() // 阻止了默认事件触发
+    this.props.form.validateFields((err, values) => {
       // 如果err存在，否则values就会获取到值
-      if(!err){
+      if (!err) {
         //  处理正常注册逻辑
-
+        this.checkuser(() => {
+          // 发起注册请求
+          console.log('123 ')
+        })
       }
     })
   }
+
+  // 点击获取验证码按钮后触发的事件
+  processsns() {
+    intervalHander = setInterval(() => {
+      timeout--
+      if (timeout <= 0) {
+        clearInterval(intervalHander)
+        timeout = 5
+        this.setState({
+          snsbttxt: '获取验证码',
+          isdisabled: false
+        })
+      } else {
+        this.setState({
+          snsbttxt: timeout + '后获取验证码',
+          isdisabled: true
+        })
+      }
+    }, 1000)
+
+    // ajax请求获取验证码
+    // this.getsnscode()
+  }
+
+  // 定义手机号是否已经被注册了逻辑
+  checkuser(callback) {
+    // 1.0 获取注册手机号码文本框的内容
+    let tel = Number(this.props.form.getFieldValue('user_name'))
+    // 2.0 将手机号码传入到服务器  /nc/common/account/checkuser
+    fetch.post('/nc/common/account/register', { username: tel }).then(res => {
+      console.log(res)
+      // return
+      if (res.status === 0) {
+        if (res.message.isRegister) {
+          // message.error('用户注册成功')
+          this.props.form.setFields({
+            ['user_name']: { value: tel, errors: [new Error('手机号码被注册')] }
+          })
+        } else {
+          //
+          if (typeof callback === 'function') {
+            callback()
+          }
+          message.error('用户注册成功')
+        }
+      } else {
+        // 跑出错误
+        console.log(res.message.text)
+      }
+    })
+    // 3.0 如果服务器响应已注册，则提示
+  }
+
+  // ajax请求获取验证码
+  getsnscode() {
+    // 获取用户在表单中输入的手机号码
+    let tel = this.props.form.getFieldValue('user_name')
+    //调用接口获取验证码
+    fetchHelper
+      .post('/nc/common/account/snscode', { username: tel })
+      .then(json => {
+        if (json.status == 1) {
+          message.error(json.message.reason || json.message)
+          return
+        }
+
+        message.success(json.message.reason)
+      })
+  }
+
+  // 定义检查密码是否一致的逻辑方法
+  checkpwd(rule,value,callback){
+    const form = this.props.form;
+    let oldvalue = this.props.form.getFieldValue('password')
+    console.log(value,oldvalue)
+    if(value&&value != oldvalue){
+      callback('两次输入的不一致')
+    }else{
+      callback()
+    }
+  }
+
   render() {
     const { getFieldDecorator } = this.props.form
     return (
@@ -41,6 +131,7 @@ class Register extends React.Component {
             <Row>
               <Col span="14">
                 <Input
+                  onBlur={this.checkuser.bind(this)}
                   ref="user_name"
                   prefix={
                     <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
@@ -83,7 +174,9 @@ class Register extends React.Component {
         </FormItem>
         <FormItem>
           {getFieldDecorator('password1', {
-            rules: [{ required: true, message: '请再一次输入密码!' }]
+            rules: [{ required: true, message: '请再一次输入密码!' },
+                      {validator:this.checkpwd.bind(this)}
+          ]
           })(
             <Input
               prefix={<Icon type="key" style={{ color: 'rgba(0,0,0,.25)' }} />}
@@ -94,11 +187,13 @@ class Register extends React.Component {
         </FormItem>
         <FormItem>
           <Button
+            disabled={this.props.disabled}
             type="primary"
+            onClick={this.processsns.bind(this)}
             htmlType="submit"
             className="login-form-button"
           >
-            注册
+            {this.state.bttxt}
           </Button>
         </FormItem>
       </Form>
